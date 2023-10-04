bdfd91a942286e4d9dacd8df916a8184455451fc
(quasar) nebula <qznebula@protonmail.com>
2023-10-04 15:24:33 -0300

    data: Tag.extraReadingURLs

--- src/data/things/art-tag.js
+++ src/data/things/art-tag.js
@@ -15,6 +15,7 @@ import {
   reverseReferenceList,
   simpleString,
   name,
+  urls,
   wikiData,
 } from '#composite/wiki-properties';
 
@@ -32,6 +33,7 @@ export class ArtTag extends Thing {
     directory: directory(),
     color: color(),
     isContentWarning: flag(false),
+    extraReadingURLs: urls(),
 
     nameShort: [
       exposeUpdateValueOrContinue({
